5c973ea40c59832ff65ffbc9c93c0d7ccccb35fb
enviel97 <nguyenvinhloc.07.10.1997@gmail.com>
2022-05-29 05:27:01 +0700

    [assets] speed up text from

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -16,8 +16,8 @@ class AppConstants {
 
   /// [BASIC SERVER]
   /// [https://be-food-app.herokuapp.com - clound host];
-  /// [http://192.168.27.27 - local host];
-  static const BASE_URL = 'https://be-food-app.herokuapp.com';
+  /// [http://192.168.27.27:3000 - local host];
+  static const BASE_URL = 'http://192.168.27.27:3000';
   static const PHOTOS = '$BASE_URL/photos';
 
   /// [FOOD ROUTE]
